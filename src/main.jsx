fdf621cc1695c15330d28e4ba4262f8479972053
Gisell0511 <giseorozco05@gmail.com>
2024-10-31 21:46:33 -0500

    correcion 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import '../src/styles/components/Main.scss'
+import '../src/styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
